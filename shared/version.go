a101ed7d919863262f30f41f9d04d9fa22981bea
Josh Lindsey <jlindsey@remedyhealthmedia.com>
2020-07-31 20:57:24 -0400

    fix assigning build info to vars

--- shared/version.go
+++ shared/version.go
@@ -1,7 +1,10 @@
 package shared
 
 var (
-	Version  string
-	Rev      string
-	BuildNum string
+	// Version is the tagged release of this binary
+	Version = "0.0.0"
+	// Rev is the git revision of the code this binary was built from
+	Rev = "local"
+	// BuildNum is the CI job number that built this binary
+	BuildNum = "N/A"
 )
